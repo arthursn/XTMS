c38395b86e4267404723bf71707846362dc51fa3
Arthur Nishikawa <nishikawa.poli@gmail.com>
2018-08-27 21:55:48 -0300

    Installation file setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cpartition',
+    version='0.1dev',
+    description='',
+    author='Arthur Nishikawa',
+    author_email='nishikawa.poli@gmail.com',
+    url='https://github.com/arthursn/cpartition',
+    packages=['rayonix'],
+    install_requires=['numpy', 'matplotlib', 'scipy']
+    # long_description=open('README.md').read(),
+)
